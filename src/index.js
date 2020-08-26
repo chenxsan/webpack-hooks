b546ff5c7dbe5ca366cfe2ebba6a2e6e1c5ee448
chenxsan <chenxsan@gmail.com>
2020-08-26 23:29:46 +0800

    intercept some compilation hooks

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import webpack from 'webpack'
 import chalk from 'chalk'
 import { HookMap } from 'tapable'
+const PluginName = 'webpack-plugin-hooks'
+const padding = 20
 class WebpackPluginHooks {
   constructor() {}
   apply(compiler) {
@@ -11,13 +13,34 @@ class WebpackPluginHooks {
     Object.keys(compiler.hooks).forEach((hook) => {
       compiler.hooks[hook].intercept({
         call: () => {
-          console.log(chalk.bgGreen('compiler.hooks'), hook)
+          console.log(chalk.bgGreen('compiler.hooks'.padStart(padding)), hook)
         },
       })
     })
 
     // TODO intercept all compilation's hooks
+    compiler.hooks.compilation.tap(PluginName, (compilation) => {
+      Object.keys(compilation.hooks).forEach((key) => {
+        const hook = compilation.hooks[key]
+        if (hook instanceof HookMap) {
+          // TODO
+        } else {
+          try {
+            hook.intercept({
+              call: () =>
+                console.log(
+                  chalk.bgCyan('compilation.hooks'.padStart(padding)),
+                  key
+                ),
+            })
+          } catch (err) {
+            // additionalchunkassets, etc.
+          }
+        }
+      })
+    })
     // how to distinguish HookMap
+    // Do I have to tap compiler.hooks.compilation to get compilation.hooks?
   }
 }
 export default WebpackPluginHooks
